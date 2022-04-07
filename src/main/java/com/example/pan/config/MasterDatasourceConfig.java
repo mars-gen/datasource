14c7489f4250b3c5f3a0dd3f63d9645a8fe18f81
13093 <123456>
2022-04-07 13:39:11 +0800

    add multiDatasource

---
+++ src/main/java/com/example/pan/config/MasterDatasourceConfig.java
@@ -0,0 +1,55 @@
+package com.example.pan.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * <p>
+ *  多数据源配置
+ * </p>
+ *
+ * @author daShen
+ * @since 2022-04-07
+ */
+@Configuration  //表示这个类为一个配置类
+@MapperScan(basePackages = "com.example.pan.mapper",sqlSessionFactoryRef = "test1SqlSessionFactory") // 配置mybatis的接口类放的地方
+public class MasterDatasourceConfig {
+
+    @Bean(name = "test1DataSource") // 将这个对象放入Spring容器中
+    @Primary  //表示这个数据源是默认数据源
+    // 读取application.properties中的配置参数映射成为一个对象
+    // prefix表示参数的前缀
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource getDateSource1() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "test1SqlSessionFactory")
+    @Primary
+    // @Qualifier表示查找Spring容器中名字为test1DataSource的对象
+    public SqlSessionFactory test1SqlSessionFactory(@Qualifier("test1DataSource") DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(
+                //设置mybatis的xml所在位置
+                new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/test01/*.xml"));
+        return bean.getObject();
+    }
+
+    @Bean(name = "test1SqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate test1sqlsessiontemplate(@Qualifier("test1SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
---
+++ src/main/java/com/example/pan/config/SlaveDatasourceConfig.java
@@ -0,0 +1,47 @@
+package com.example.pan.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author daShen
+ * @since 2022-04-07
+ */
+@Configuration
+@MapperScan(basePackages = "com.example.pan.mapper.test02",sqlSessionFactoryRef = "")
+public class SlaveDatasourceConfig {
+
+    @Bean(name = "test2DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    public DataSource getDateSource2() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "test2SqlSessionFactory")
+    public SqlSessionFactory test2SqlSessionFactory(@Qualifier("test2DataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/test02/*.xml"));
+        return bean.getObject();
+    }
+
+    @Bean(name = "SqlSessionTemplate")
+    public SqlSessionTemplate test2sqlsessiontemplate(@Qualifier("test2SqlSessionFactory") SqlSessionFactory sessionFactory) {
+        return new SqlSessionTemplate(sessionFactory);
+    }
+}
